df07122693e8d28938747676527c35a9744b4246
emilyshou <emihh.gamer@gmail.com>
2025-08-20 11:43:12 -0300

    Update Condicionais1.c

--- Condicionais1.c
+++ Condicionais1.c
@@ -2,3 +2,17 @@
 #include<locale.h>
 int main()
 {
+    setlocale(LC_ALL, "Portuguese");
+    int alunos, monitores;
+    scanf("%d %d", &alunos, &monitores);
+
+    if (alunos + monitores <= 50) {
+        printf("É possível levar todos os alunos e
+monitores em apenas uma viagem\n");
+    } else {
+        printf("Não é possível levar todos os alunos e
+monitores em apenas uma viagem\n");
+    }
+
+    return 0;
+}
