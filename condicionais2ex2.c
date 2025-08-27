e517d533ea0685329062e7c85347005e02a8bbda
emilyshou <emihh.gamer@gmail.com>
2025-08-27 10:15:24 -0300

    Update condicionais2ex2.c

--- condicionais2ex2.c
+++ condicionais2ex2.c
@@ -6,29 +6,34 @@ int main() {
     float v1, v2, v3, media;
 
     printf("Digite a opção (A - aritmética, P - ponderada, G - geométrica, H - harmônica): ");
-    while ((opcao = getchar()) == '\n' || opcao == ' ');
+    scanf(" %c", &opcao);
+
+       if (opcao != 'A' && opcao != 'a' &&
+        opcao != 'P' && opcao != 'p' &&
+        opcao != 'G' && opcao != 'g' &&
+        opcao != 'H' && opcao != 'h') {
+        printf("Opção inválida!\n");
+        return 0;
+    }
 
     printf("Digite três valores: ");
     scanf("%f %f %f", &v1, &v2, &v3);
 
-    if (opcao != 'A' && opcao != 'a'){
-            media = (v1 + v2 + v3) / 3.0;
-            printf("Média aritmética: %.2f\n", media);
+    if (opcao == 'A' || opcao == 'a') {
+        media = (v1 + v2 + v3) / 3.0;
+        printf("Média aritmética: %.2f\n", media);
     }
-    else if (opcao != 'P' && opcao != 'p'){
-            media = (v1*1 + v2*2 + v3*3) / 6.0;
-            printf("Média ponderada: %.2f\n", media);
+    else if (opcao == 'P' || opcao == 'p') {
+        media = (v1*1 + v2*2 + v3*3) / 6.0;
+        printf("Média ponderada: %.2f\n", media);
     }
-    else if (opcao != 'G' && opcao != 'g'){
-            media = cbrtf(v1 * v2 * v3);
-            printf("Média geométrica: %.2f\n", media);
+    else if (opcao == 'G' || opcao == 'g') {
+        media = cbrtf(v1 * v2 * v3);
+        printf("Média geométrica: %.2f\n", media);
     }
-    else if (opcao != 'H' && opcao != 'h'){
-            media = 3.0 / (1.0/v1 + 1.0/v2 + 1.0/v3);
-            printf("Média harmônica: %.2f\n", media);
-    }
-    else {
-            printf("Opção inválida!\n");
+    else if (opcao == 'H' || opcao == 'h') {
+        media = 3.0 / (1.0/v1 + 1.0/v2 + 1.0/v3);
+        printf("Média harmônica: %.2f\n", media);
     }
 
     return 0;
