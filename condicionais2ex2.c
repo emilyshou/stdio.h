71bc6063f83a27d4e44f5a6576e3f878f1b50eee
emilyshou <emihh.gamer@gmail.com>
2025-08-27 09:13:09 -0300

    Add files via upload

---
+++ condicionais2ex2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    char opcao;
+    float v1, v2, v3, media;
+
+    printf("Digite a opção (A - aritmética, P - ponderada, G - geométrica, H - harmônica): ");
+    scanf(" %c", &opcao);
+
+    printf("Digite três valores: ");
+    scanf("%f %f %f", &v1, &v2, &v3);
+
+    switch(opcao) {
+        case 'A':
+        case 'a':
+            media = (v1 + v2 + v3) / 3.0;
+            printf("Média aritmética: %.2f\n", media);
+            break;
+        case 'P':
+        case 'p':
+            media = (v1*1 + v2*2 + v3*3) / 6.0;
+            printf("Média ponderada: %.2f\n", media);
+            break;
+        case 'G':
+        case 'g':
+            media = cbrtf(v1 * v2 * v3);
+            printf("Média geométrica: %.2f\n", media);
+            break;
+        case 'H':
+        case 'h':
+            media = 3.0 / (1.0/v1 + 1.0/v2 + 1.0/v3);
+            printf("Média harmônica: %.2f\n", media);
+            break;
+        default:
+            printf("Opção inválida!\n");
+    }
+
+    return 0;
+}
